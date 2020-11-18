a3bef9938d01393af77745b2a42a3e8f3088485c
Jesper Berg <jesper.berg@mailbox.org>
2020-11-18 23:55:26 +0100

    not working

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -2,14 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/get', function(req, res, next) {
-  res.render('index', { title: 'Get Chat' });
+router.get('/get', function (req, res, next) {
+    res.render('index', { title: 'Get Chat' });
 });
 /* GET home page. */
-router.get('/set', function(req, res, next) {
-  res.render('index', { title: 'Set Chat' });
+router.get('/set', function (req, res, next) {
+    let testmessage = "This is a test";
+    isNotWord(testmessage);
+    res.render('index', { title: 'Set Chat' });
 });
+function isNotWord(message) {
+    let messageArr = message.split(/[ ]+/);
+    console.log(messageArr);
+    let cleanMessageArr = messageArr.map(m => m.replace(/[^\W\s]/gi, ''));
 
-
+}
 
 module.exports = router;
