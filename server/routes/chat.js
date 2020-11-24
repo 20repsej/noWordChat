400dc9c050b5475809ea7fc87578cdc64fcc1355
Jesper <jesper.berg@mailbox.org>
2020-11-24 08:25:31 +0100

    jlsadkjfl;

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -5,10 +5,8 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    let testobj = {'time':'2020', 'text':'mitt fina meddelande att skicka'};
-    fs.appendFile('chat.json', ',\n' + JSON.stringify(testobj), function upd() {
-        console.log("TODO error if error");
-    });
+
+
     res.render('index', { title: 'Get Chat' });
 });
 
@@ -17,6 +15,10 @@ function isNotWord(message) {
     console.log(messageArr);
     let cleanMessageArr = messageArr.map(m => m.replace(/[^\W\s]/gi, ''));
 
+    let testobj = {'time':'2020', 'text':'mitt fina meddelande att skicka'};
+    fs.appendFile('chat.json', ',\n' + JSON.stringify(testobj), function upd() {
+        console.log("TODO error if error");
+    });
 }
 
 module.exports = router;
