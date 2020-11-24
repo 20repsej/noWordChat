9244d4df45505db4d6bab1d60ab738a0f84f3e86
Jesper <jesper.berg@mailbox.org>
2020-11-24 08:54:21 +0100

    not working client

--- client/console/Program.cs
+++ client/console/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace console
 {
     class Program
     {
+
+        public class message
+        {
+            public string username;
+            public string userMessage;
+        }
+        public class Messages
+        {
+            public Messages[] messages;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WebClient wc = new WebClient();
+            string json = wc.DownloadString("http://10.254.144.84:3000/chat/get");
+
+            Messages allMessages = JsonConvert.DeserializeObject<Messages>(json);
+
+            for (int i = 0; i < allMessages.messages.Length; i++)
+                System.Console.WriteLine("Data" + allMessages.messages[i]);
+            {
+                
+            }
         }
     }
 }
