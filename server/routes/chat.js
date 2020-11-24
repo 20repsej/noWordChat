a29d49708d13dcef6da99f32ac47682eb207530a
Axel <axel.wennstroem@gmail.com>
2020-11-24 09:01:17 +0100

    asödkjf

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var chat = require('./chat.json');
 
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/get', function (req, res, next) {
+    
 
+    res.send(chat);
+});
+
+router.get('/set', function (req, res, next) {
+    let username = req.query.username;
+    let messageText = req.query.messageText;
+
+    let message = JSON.parse(username, messageText);
+
+    chat.messages.push(message);
 
     res.render('index', { title: 'Get Chat' });
 });
+
+
 
 function isNotWord(message) {
     let messageArr = message.split(/[ ]+/);
